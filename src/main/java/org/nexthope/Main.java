a32dd11cd270e698e39355bd9d99402fd2d679d3
aminhl <amin.hlel@esprit.tn>
2026-01-10 21:59:00 +0100

    feat: resolve validAnagram brute force & reset main method

--- src/main/java/org/nexthope/Main.java
+++ src/main/java/org/nexthope/Main.java
@@ -1,15 +1,9 @@
 package org.nexthope;
 
-import org.nexthope.arrays.ContainsDuplicate;
-import org.nexthope.arrays.ValidAnagram;
-
 public class Main {
 
     public static void main(String[] args) {
-        /* arrays & hashing */
-        ContainsDuplicate.hasDuplicate(new int[]{1, 2, 3, 3});
-        ValidAnagram.isAnagram("racecar","carrace");
-
+        System.out.println("Hello, world!");
     }
 
 }
--- src/main/java/org/nexthope/arrays/ValidAnagram.java
+++ src/main/java/org/nexthope/arrays/ValidAnagram.java
@@ -1,19 +1,28 @@
 package org.nexthope.arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ValidAnagram {
 
+    public static boolean isAnagramBf(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+
     public static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        Map<Character, Integer> sMap = new HashMap<>();
-        Map<Character, Integer> tMap = new HashMap<>();
+       if (s.length() != t.length()) return false;
+       Map<Character, Integer> mapS = new HashMap<>();
+       Map<Character, Integer> mapT = new HashMap<>();
         for (Character c : s.toCharArray())
-            sMap.put(c, sMap.getOrDefault(c,0)+1);
+            mapS.put(c, mapS.getOrDefault(c, 0)+1);
         for (Character c : t.toCharArray())
-            tMap.put(c, tMap.getOrDefault(c,0)+1);
-        return sMap.equals(tMap);
+            mapT.put(c, mapT.getOrDefault(c, 0)+1);
+        return mapS.equals(mapT);
     }
 
 }
