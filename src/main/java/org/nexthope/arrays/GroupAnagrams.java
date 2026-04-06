72d51eb12ba9dba9f86bfcb139ebfc146ad71e46
aminhl <amin.hlel@esprit.tn>
2026-04-06 16:41:16 +0200

    feat: solve GroupAnagrams with hash table

--- src/main/java/org/nexthope/arrays/GroupAnagrams.java
+++ src/main/java/org/nexthope/arrays/GroupAnagrams.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class GroupAnagrams {
 
-    public static List<List<String>> groupAnagrams(String[] strs) {
+    public static List<List<String>> groupAnagramsBf(String[] strs) {
         Map<String, List<String>> result = new HashMap<>();
         for (String s : strs) {
             char[] chars = s.toCharArray();
@@ -13,6 +13,18 @@ public class GroupAnagrams {
             result.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
         return result.values().stream().toList();
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            int[] hashKey = new int[26];
+            for (char c : s.toCharArray())
+                hashKey[c - 'a']++;
+            String key = Arrays.toString(hashKey);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return map.values().stream().toList();
     }
 
 }
