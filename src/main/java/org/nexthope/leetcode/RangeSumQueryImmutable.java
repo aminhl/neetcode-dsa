649e007aa20e48112b85dbfcac54ba3accc70a2e
aminhl <amin.hlel@esprit.tn>
2026-04-19 19:02:31 +0200

    leetcode 303: range sum query immutable

---
+++ src/main/java/org/nexthope/leetcode/RangeSumQueryImmutable.java
@@ -0,0 +1,25 @@
+package org.nexthope.leetcode;
+
+public class RangeSumQueryImmutable {
+
+    public static class NumArray {
+
+        private final int[] prefix;
+
+        public NumArray(int[] nums) {
+            int n = nums.length;
+            prefix = new int[n];
+            prefix[0] = nums[0];
+            for (int i = 1; i <n ; i++)
+                prefix[i] = prefix[i-1]+nums[i];
+        }
+
+        public int sumRange(int left, int right) {
+            if (left == 0)
+                return prefix[right];
+            return prefix[right] - prefix[left-1];
+        }
+
+    }
+
+}
